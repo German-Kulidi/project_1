6378a51f1be9badd33b157131beb45e0597f302d
German <ger.kulidi@gmail.com>
2023-01-18 02:16:59 +0400

    second commit

--- js/script.js
+++ js/script.js
@@ -10,12 +10,41 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = prompt('На сколько оцените его?', '');
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const film = prompt('Один из последних просмотренных фильмов?', '');
+    const rating = prompt('На сколько оцените его?', '');
+
+    if (film != null && rating != null && film != '' && rating !='' && film.length < 50) {
+        personalMovieDB.movies[film] = rating;       
+    } else {
+        i--;
+    }
+}
+
+// let i = 0;
+// do {
+//     i++;
+//     const film = prompt('Один из последних просмотренных фильмов?', '');
+//     const rating = prompt('На сколько оцените его?', '');
+
+//     if (film != null && rating != null && film != '' && rating !='' && film.length < 50) {
+//         personalMovieDB.movies[film] = rating;       
+//     } else {
+//         i--;
+//     }
+// } while (i < 2);
+
+const countFilms = personalMovieDB.count;
+
+if (countFilms < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (countFilms >= 10 && countFilms < 30 ) {
+    alert('Вы классический зритель');
+} else if (countFilms > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
